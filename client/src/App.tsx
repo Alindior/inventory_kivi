0819a04bbb782a19a2798a3c22b7eea478a55d0c
Andrew Vavilau <alindior123@gmail.com>
2022-07-18 21:10:26 +0300

    add-hello-world

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC<Props> = ({ updateAuth, auth }) => {
   useEffect(() => {
     updateAuth();
   }, []);
-
+  HELLO JOPA!!!
   return (
     <Router history={history}>
       <Switch>
